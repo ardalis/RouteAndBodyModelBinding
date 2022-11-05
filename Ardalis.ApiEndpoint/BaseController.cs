3b7e4f515d5a1a446c35247083aa050fffba4c63
chandrakanth <p.chan20new@gmail.com>
2022-11-05 18:15:52 +0530

    Added attribute setup with controller action
    Resolve: https://github.com/ardalis/RouteAndBodyModelBinding/issues/1

---
+++ Ardalis.ApiEndpoint/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Ardalis.ApiEndpoint;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseController : ControllerBase
+{
+	[HttpGet("modelbinding/{id}")]
+	public IActionResult WithModelBinding([FromModel] DemoDynamicModel demoModel)
+	{
+		return StatusCode(StatusCodes.Status204NoContent);
+	}
+}
